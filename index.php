e469fb6022cfd4820e8b92bba7023e50edb767a8
intanrsp <126649690+intanrsp@users.noreply.github.com>
2023-03-06 19:28:24 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Happy Birthday | Party Girl</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="icon" type="image/png" href="images/Happy.png"/>
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+
+	<link rel="stylesheet" type="text/css" href="styles/util.css">
+	<link rel="stylesheet" type="text/css" href="styles/data.css">
+
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100" style="background-image: url('images/ultah3.jpg');">
+			<div class="wrap-login100 p-t-30 p-b-50">
+				<span class="login100-form-title p-b-41">
+					PLEASE FILL YOUR DATA!
+				</span>
+				<form class="login100-form validate-form p-b-33 p-t-5" method="POST" action="hasil.php">
+
+				<tr>
+					<div class="wrap-input100 validate-input">
+						<td><input class="input100" type="text" name="nama" placeholder="Your Name"></td>
+						<span class="focus-input100" data-placeholder="&#xe82a;"></span>
+					</div>
+				</tr>
+
+				<tr>
+					<div class="wrap-input100 validate-input">
+						<td><input class="input100" type="date" name="birth"></td>
+						<span class="focus-input100" data-placeholder="&#xe836;"></span>
+					</div>
+				</tr>
+
+					<div class="container-login100-form-btn m-t-32">
+						<button class="login100-form-btn">
+							SEND
+						</button>
+					</div>
+
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+	<script src="vendor/select2/select2.min.js"></script>
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+	<script src="script/java.js"></script>
+
+</body>
+</html>
